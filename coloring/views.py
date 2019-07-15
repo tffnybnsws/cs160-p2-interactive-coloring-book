08ff2763cbb0c5f0f02eae23e233106db1063f3a
trang.lt157 <trang.lt157@gmail.com>
2019-07-15 01:40:13 -0400

    add undo button

--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+  
+#def palette(request):
+ #   return render(request, 'coloring/palette.html')
--- p3/urls.py
+++ p3/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('coloring/', include('coloring.urls')),
+    #path('coloring/palette', include('coloring.urls'))
 ]
