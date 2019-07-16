7a4ef6b8cdc86e8a35e90d48f9e19ce9bc5bb537
Trang Le <tranglet@vn.fujitsu.com>
2019-07-16 09:55:57 -0700

    merge all features

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+  path('search/', views.search, name='search'),
+  path('archive/', views.archive, name='archive'),
+    path('', views.index, name='index'), 
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+def search(request):
+    return render(request, 'coloring/search.html')
+  
+def archive(request):
+    return render(request, 'coloring/archive.html')
+  
 def index(request):
     return render(request, 'coloring/index.html')
-  
-#def palette(request):
- #   return render(request, 'coloring/palette.html')
