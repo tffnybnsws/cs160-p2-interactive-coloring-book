1869ad67924c51a4719afeb14188324743b0f573
tboonsawas <tboonsawas@gmail.com>
2019-07-17 02:59:19 -0400

    Deleted unwanted html files

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('aphex/', views.aphex, name='aphex'),
   path('search/', views.search, name='search'),
   path('archive/', views.archive, name='archive'),
-  path('aphextwin/', views.aphextwin, name='aphextwin'),
-  path('ddealer', views.ddealer, name='ddealer'),
   path('', views.index, name='index'), 
 ]
